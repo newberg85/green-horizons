0b77111f8ee5aa8ec9599bffeb115e582d93b156
newberg85 <bergviana12345@gmail.com>
2025-08-12 16:04:53 -0300

    adicionei a página contato

---
+++ src/app/contato/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+
+export default function Contato() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2 sm:p-24 sm:pb-0 pb-0 ">
+      <Header />
+      <Footer />
+    </main>
+  )
+}
